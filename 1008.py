c4c69f8e954b22a06ad3d8ef205ac5206083a791
Isabelle Mosca <bellemosca@gmail.com>
2025-01-05 09:52:05 -0300

    add salary calculation for employee based on hours worked

---
+++ 1008.py
@@ -0,0 +1,8 @@
+n_funcionario = int(input())
+h_trabalhadas = int(input())
+recebe_por_hora = float(input())
+
+salario = h_trabalhadas * recebe_por_hora
+
+print(f"NUMBER = {n_funcionario}")
+print(f"SALARY = U$ {salario:.2f}")
